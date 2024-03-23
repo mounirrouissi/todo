912b1d73e013586d5124d5129b2a5923c939afd3
Monir11 <mounirrouissi2@gmail.com>
2024-03-23 14:01:20 +0100

    add test profile

--- src/test/java/com/todo/demo/DemoApplicationTests.java
+++ src/test/java/com/todo/demo/DemoApplicationTests.java
@@ -2,8 +2,10 @@ package com.todo.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // Use the "test" profile for test-specific configurations
 class DemoApplicationTests {
 
 	@Test
